8d90445ac3a418280afd31d3c34c219af2ac29ef
renanssantos <renansilva97.rs@gmail.com>
2014-11-17 17:51:07 -0200

    Site estabilizado

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://127.0.0.1/sitApTecVesp2014/';
+$config['base_url']	= 'http://127.0.0.1/Situacao_Aprendizagem_Renan/';
 
 /*
 |--------------------------------------------------------------------------
